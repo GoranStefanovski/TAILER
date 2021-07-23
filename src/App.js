60d6f318bad62cc203486f9c9732ad356d1394d4
GoranStefanovski <tevidma@gmail.com>
2021-07-23 13:47:30 +0200

    adding packages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-
+import './css/front/app.scss';
+// import { tns } from "tiny-slider"
 import api from './services/api'
 import React, { useState, useEffect } from 'react';
-
+// import {tns} from './src/tiny-slider.js';
 function App() {
   const [media, setMedia] = useState([]);
-
   // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
     const ass = async () => {
@@ -13,16 +13,17 @@ function App() {
       setMedia(apiMedia)
     }
     ass()
-  }, [])
+  }, []);
 
   return (
     <div className="App">
+    <slider></slider>
       <header className="App-header">
         {
           media.map((mediaItem, idx) => {
             return <div key={idx}>
               <h4>{mediaItem.title}</h4>
-              <img width="100" src={mediaItem.url}/>
+              <img width="100" src={mediaItem.url} alt={mediaItem.id}/>
             </div>
           })
         }
