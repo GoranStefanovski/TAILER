8db2b2db7b307fd0f5aacfbbcd6fd26dd6151e70
GoranStefanovski <tevidma@gmail.com>
2021-07-23 14:10:33 +0200

    conf foront

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
 import './App.css';
-import './css/front/app.scss';
-// import { tns } from "tiny-slider"
-import api from './services/api'
-import React, { useState, useEffect } from 'react';
-// import {tns} from './src/tiny-slider.js';
+import React from 'react';
+
 function App() {
-  const [media, setMedia] = useState([]);
-  // Similar to componentDidMount and componentDidUpdate:
-  useEffect(() => {
-    const ass = async () => {
-      const apiMedia = await api.getMedia()
-      setMedia(apiMedia)
-    }
-    ass()
-  }, []);
+
 
   return (
     <div className="App">
-    <slider></slider>
-      <header className="App-header">
-        {
-          media.map((mediaItem, idx) => {
-            return <div key={idx}>
-              <h4>{mediaItem.title}</h4>
-              <img width="100" src={mediaItem.url} alt={mediaItem.id}/>
-            </div>
-          })
-        }
-      </header>
+
+
     </div>
   );
 }
