ff5604698177fc09223e4b933dc0620258436ec5
GoranStefanovski <tevidma@gmail.com>
2021-07-29 11:31:00 +0200

    Refactoring Css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import './App.css';
 import React from 'react';
-// import Comercial from './Images/commercial.jpg';
-// import Technical from './Images/Technical.jpg';
-// import Personal from './Images/Personal.jpg';
 import GuestDefaultPage from './react/layouts/GuestDefaultPage.js';
-
-
-
+import './css/front/app.scss';
 
 function App() {
     return (
--- src/react/layouts/GuestDefaultPage.js
+++ src/react/layouts/GuestDefaultPage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './GuestDefaultPage.css';
 import TinySlider from "tiny-slider-react";
 
+// import Slider from "../../components/Slider /Slider";
+
 function GuestDefaultPage() {
     const settings = {
         lazyload: true,
@@ -11,24 +13,47 @@ function GuestDefaultPage() {
         preventScrollOnTouch: 'auto',
     };
 
+    // const imgs = [
+    //     {
+    //         style: {
+    //             backgroundImage: `url("Images/commercial.jpg")`,
+    //             height: '100vh',
+    //         }
+    //         title: 'one"
+    //     },
 
     // sega ke bidi array = {stil =>{}, naslov = "BlaBla"}
-    const imgs = [{
-        backgroundImage: `url("Images/commercial.jpg")`,
-        height: '100vh',
-    }, {
-        backgroundImage: `url("Images/Technical.jpg")`,
-        height: '100vh'
-    }, {
-        backgroundImage: `url("Images/Personal.jpg")`,
-        height: '100vh'
-    }];
+    const imgs = [
+        {
+            style: {
+                backgroundImage: `url("Images/commercial.jpg")`,
+                height: '100vh',
+            },
+            title: "Commercial"
+        },
+        {
+            style: {
+                backgroundImage: `url("Images/Technical.jpg")`,
+                height: '100vh',
+            },
+            title: "Techical"
+        },
+        {
+            style: {
+                backgroundImage: `url("Images/Personal.jpg")`,
+                height: '100vh'
+            },
+            title: "Personal"
+
+        }];
 
     return <div>
         <TinySlider settings={settings}>
             {imgs.map((el, index) => (
                 <div key={index} style={{position: "relative"}}>
-                    <div style={el}>
+                    <div style={el.style}>
+                        {/*<Slider/>*/}
+                        <h1 className="header">{el.title}</h1>
                     </div>
                 </div>
             ))}
