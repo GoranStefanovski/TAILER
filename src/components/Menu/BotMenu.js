7b726d95f23a97599bbb02d6f804ba640e2be6c2
GoranStefanovski <tevidma@gmail.com>
2021-07-30 13:20:05 +0200

    fixing scss

---
+++ src/components/Menu/BotMenu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BotMenu(text, buttonLink) {
+    return <div className="botMenu">
+        <div className="botMenu__left">
+            <h1>{text}</h1>
+        </div>
+    </div>
+}
+
+export default BotMenu;
---
+++ src/components/Menu/TopMenu.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function TopMenu() {
+    return <div className="topMenu">
+        <div className="logoImage">
+        {/*  Image  */}
+        </div>
+        <div className="navMenu">
+            {/*  Full screen Overlay Navigation  */}
+        </div>
+    </div>
+}
+
+export default TopMenu;
