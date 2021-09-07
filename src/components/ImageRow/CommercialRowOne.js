7eef2d98606f5f77a2fb3fc39cab525ffd215f72
GoranStefanovski <tevidma@gmail.com>
2021-09-07 16:41:07 +0200

    Finishing Commercial

--- src/components/ImageRow/CommercialRowOne.js
+++ src/components/ImageRow/CommercialRowOne.js
@@ -1,24 +1,76 @@
 import React from 'react';
+//
+// const top = {
+//     top: '3717px'
+// }
 
 function CommercialRowOne() {
     return (
         <div>
             <div className="CommercialRowOne">
-                <img src="Images/commercial/M2.jpg" alt="Ovaj"/>
-                <img id="two" src="Images/commercial/Rossetto2b_1.jpg" alt="Ovaj"/>
-                <img src="Images/commercial/Social_02.jpg" alt="Ovaj"/>
-                <img src="Images/commercial/Op_1.jpg" alt="Ovaj"/>
+                <img className="CommImageOne" src="Images/commercial/M2.jpg" alt="Ovaj"/>
+                <img className="CommImageTwo" src="Images/commercial/Rossetto2b_1.jpg" alt="Ovaj"/>
+                <img className="CommImageThree" src="Images/commercial/Social_02.jpg" alt="Ovaj"/>
+                <img className="CommImageFour" src="Images/commercial/Op_1.jpg" alt="Ovaj"/>
             </div>
-            <div className="CommercialRowOne marginTop ">
-                <img src="Images/commercial/5_Def.jpg" alt="Ovaj"/>
-                <img id="twoSecond" src="Images/commercial/Eye4.jpg" alt="Ovaj"/>
-                <img src="Images/commercial/Malo_05.jpg" alt="Ovaj"/>
+            <div className="CommercialRowOne">
+                <img className="CommImageFive" src="Images/commercial/5_Def.jpg" alt="Ovaj"/>
+                <img className="CommImageSix" src="Images/commercial/Eye4.jpg" alt="Ovaj"/>
+                <img className="CommImageSeven" src="Images/commercial/Malo_05.jpg" alt="Ovaj"/>
             </div>
 
-            <div className="CommercialRowThree">
-                <div className="col-md-8">
-                
+            <div className="CommercialText">
+                <div>
+                    <h1>TAILER CREATIVE</h1>
+                    <h1><span>IMMAGINE</span></h1>
+                    <h5>Tailer fornisce un contributo completo ad i brand che ci contattano per valorizzare i propri
+                        prodotti, si differenzia dai competitor per la ricerca creativa e la sensibilità di lettura del
+                        prodotto. Lo studio può lavorare in collaborazione con il tema Marketing del brand, o
+                        contribuire
+                        con servizi di consulenza creativa. Le attività dello studio sono organizzate per la creazione
+                        di
+                        immagini video e fotografiche destinate al modo social advertising e</h5>
                 </div>
+            </div>
+            <div className="CommercialRowOne">
+                <img className="CommImageSecondOne" src="Images/commercial/Mesauda_1.jpg" alt="Ovaj"/>
+                <div className="gridSystem">
+                    <img className="CommImageSecondTwo" src="Images/commercial/H3.jpg" alt="Ovaj"/>
+                    <img className="CommImageSecondThree" src="Images/commercial/Profumo1.jpg" alt="Ovaj"/>
+                </div>
+                <img className="CommImageSecondFour" src="Images/commercial/B1.jpg" alt="Ovaj"/>
+            </div>
+            <div className="CommercialRowOne Height">
+                <div className="gridSystem">
+                    <img className="CommImageSecondFive" src="Images/commercial/Malo_17.jpg" alt="Ovaj"/>
+                    <img className="CommImageSecondEight" src="Images/commercial/Box_Penne_2800.jpg" alt="Ovaj"/>
+                </div>
+                <div className="gridSystem">
+                    <img className="CommImageSecondSix" src="Images/commercial/Radiance_2.jpg" alt="Ovaj"/>
+                    <img className="CommImageSecondNine" src="Images/commercial/Aqua_1.jpg" alt="Ovaj"/>
+                </div>
+                <div className="gridSystem">
+                    <img className="CommImageSecondSeven" src="Images/commercial/Medavita_Adv_C.jpg" alt="Ovaj"/>
+                </div>
+            </div>
+
+            <div className="CommercialInfo">
+                <div className="logoColorDiv">
+                    <img className="logoColor" src="Images/logoColor.png" alt="Tailer Logo"/>
+                </div>
+                <div className="CommercialInfoText">
+                    <h5>Lorem ipsun dolor sit ament, consectur ament elite.</h5>
+                        <h5>Ust de labore aqua aliqua</h5>
+                    <h6>Office: + 1 (111) 11-11-11</h6>
+                    <h6>example@gmail.com</h6>
+                </div>
+            </div>
+
+            <div className="CommercialFooter">
+                <img src="Images/tech/L_D_2.jpg" alt="Ovaj" />
+                <img src="Images/tech/L_D_5.jpg" alt="Ovaj" />
+                <img src="Images/tech/L_D_6.jpg" alt="Ovaj" />
+                <img src="Images/tech/L_D_6b.jpg" alt="Ovaj" />
             </div>
         </div>
     )
