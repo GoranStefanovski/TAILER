c9e75bf94cb4e0994751c70583e4f2cba3090d24
GoranStefanovski <tevidma@gmail.com>
2021-09-09 17:02:30 +0200

    Finishing Personal

--- src/react/front/views/Personal.js
+++ src/react/front/views/Personal.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Header from "../../../components/Header/Header";
 
-// const imagePersonal = {
-//     backgroundImage: `url("Images/Commercial.jpg")`,
-//     height: '100vh',
-//     position: 'absolute',
-// }
+const imagePersonal = {
+    backgroundImage: `url("Images/Personal.jpg")`,
+    height: '100vh',
+    position: 'absolute',
+    // paddingTop: '1%'
+}
 
 function Personal(){
     return (
-        <div className="personal" >
+        <div className="personal" style={imagePersonal}>
             <Header/>
-            <div className="CommercialInfo">
-                <div className="logoColorDiv">
+            <div className="CommercialInfo bgDark">
+                <div className="logoColorDiv paddingTop">
                     <img className="logoColor" src="Images/logoColor.png" alt="Tailer Logo"/>
                 </div>
                 <div className="CommercialInfoText">
