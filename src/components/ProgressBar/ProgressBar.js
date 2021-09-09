ccac3252a0bf7b702f0f2e4864de88b91d1de921
unknown <ugrenovikj.stefan@gmail.com>
2021-09-09 17:30:48 +0200

    Progress Circles

---
+++ src/components/ProgressBar/ProgressBar.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
+import { easeQuadInOut } from 'd3-ease'
+import AnimatedProgressProvider from './AnimatedProgressProvider'
+
+
+
+
+function ProgressBar({valueEndProp}) {
+
+    return (
+        
+        <div >
+            
+            <div style={{ width: 200, height: 200 }}>
+            
+
+                <AnimatedProgressProvider
+                valueStart={0}
+                valueEnd={valueEndProp}
+                duration={1.4}
+                easingFunction={easeQuadInOut}
+                >
+                {(value) => {
+                    const roundedValue = Math.round(value);
+                    return (
+                    <CircularProgressbar
+                        value={value}
+                        text={`${roundedValue}%`}
+                        /* This is important to include, because if you're fully managing the
+                        animation yourself, you'll want to disable the CSS animation. */
+                        styles={buildStyles({ pathTransition: 'none' })}
+                    />
+                    );
+                }}
+                </AnimatedProgressProvider>;
+            </div>
+        </div>
+    )
+}
+
+export default ProgressBar
